d82777d5fcb10c25ae9811593a753801223756a3
TheHackerOne <yash2200@gmail.com>
2020-09-14 22:50:21 +0530

    updated

--- server.js
+++ server.js
@@ -21,14 +21,14 @@ app.use('/blog', require("./routes/blogPost"))
 
 const PORT = process.env.PORT||5000;
 
-if(process.env.NODE_ENV === "production"){
+// if(process.env.NODE_ENV === "production"){
 
-  app.use(express.static('client/build'));
+//   app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   })
+// }
 
 app.listen(PORT, () => {
   console.log(`Succesfully connected to port ${PORT}`)
